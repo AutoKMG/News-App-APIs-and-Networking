2bcefb11e73b3359ce880ebbe7d7ba873ec46aa8
Khaled Saleh <khaled@connou.app>
2023-03-06 22:11:08 +0100

    the main scaffold

---
+++ lib/layout/news_app/news_layout.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/shared/logic/news/handler.dart';
+
+class NewsLayout extends StatelessWidget {
+  const NewsLayout({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    NewsHandler newsHandler = BlocProvider.of<NewsHandler>(context);
+    return BlocConsumer<NewsHandler, NewsState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("News App"),
+            actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+          ),
+          body: newsHandler.screens[newsHandler.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            items: newsHandler.bottomNavItems,
+            currentIndex: newsHandler.currentIndex,
+            onTap: (value) {
+              newsHandler.changeIndex(value);
+            },
+          ),
+        );
+      },
+    );
+  }
+}
