c89ab9ca33529d8b6dbf30e6adccea5ca8d45c69
Khaled Saleh <khaled@connou.app>
2023-03-06 22:09:14 +0100

    the app main screens

---
+++ lib/modules/science/science_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScienceScreen extends StatelessWidget {
+  const ScienceScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: const Text("Hello Science!"));
+  }
+}
