73ceb64e2e13c00986c8af061b7e6f7495e10072
Khaled Saleh <khaled@connou.app>
2023-03-08 20:51:59 +0100

    reusable article builder and dividor widgets

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -71,6 +71,69 @@ Widget defaultFormField({
         border: OutlineInputBorder(),
       ),
     );
+
+Widget buildArticleItem() {
+  return Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: Row(
+      children: [
+        Container(
+          width: 120,
+          height: 120,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            image: DecorationImage(
+              image: NetworkImage('https://i.imgur.com/kmryMrZ.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          child: SizedBox(
+            height: 120,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Text(
+                    "Title",
+                    maxLines: 4,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                Text(
+                  "2021-04-02T11:43:00Z",
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget myDividor() {
+  return Padding(
+    padding: EdgeInsetsDirectional.only(start: 20),
+    child: Container(
+      width: double.infinity,
+      height: 1,
+      color: Colors.grey[300],
+    ),
+  );
+}
 /*
 Widget buildTaskItem(Map model, context) {
   AppHandler appHandler = BlocProvider.of<AppHandler>(context);
