f38f82555d87d74862d04ea4b214295763f44f55
Khaled Saleh <khaled@connou.app>
2023-03-08 20:53:30 +0100

    removing floating action button

--- lib/layout/news_app/news_layout.dart
+++ lib/layout/news_app/news_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/shared/logic/news/handler.dart';
+import 'package:news_app/shared/network/remote/dio_helper.dart';
 
 class NewsLayout extends StatelessWidget {
   const NewsLayout({Key key}) : super(key: key);
@@ -9,14 +10,16 @@ class NewsLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     NewsHandler newsHandler = BlocProvider.of<NewsHandler>(context);
     return BlocConsumer<NewsHandler, NewsState>(
-      listener: (context, state) {
-        // TODO: implement listener
-      },
+      listener: (context, state) {},
       builder: (context, state) {
         return Scaffold(
           appBar: AppBar(
             title: Text("News App"),
             actions: [IconButton(onPressed: () {}, icon: Icon(Icons.search))],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
           ),
           body: newsHandler.screens[newsHandler.currentIndex],
           bottomNavigationBar: BottomNavigationBar(
