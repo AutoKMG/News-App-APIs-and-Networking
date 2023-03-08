9a99e38812955bcc3c0490f7c81558cf673391b2
Khaled Saleh <khaled@connou.app>
2023-03-08 21:54:25 +0100

    creating the functions for getting sports and science data

--- lib/shared/logic/news/handler.dart
+++ lib/shared/logic/news/handler.dart
@@ -1,6 +1,5 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:meta/meta.dart';
 import 'package:news_app/modules/business/business_screen.dart';
 import 'package:news_app/modules/science/science_screen.dart';
 import 'package:news_app/modules/settings/settings_screen.dart';
@@ -10,10 +9,16 @@ import 'package:news_app/shared/network/remote/dio_helper.dart';
 part 'state.dart';
 
 class NewsHandler extends Cubit<NewsState> {
-  NewsHandler() : super(NewsStateInitial());
+  NewsHandler() : super(NewsStateInitial()) {
+    getBusinessData();
+    getSportsData();
+    getScienceData();
+  }
 
   int currentIndex = 0;
   List<dynamic> business = [];
+  List<dynamic> sports = [];
+  List<dynamic> science = [];
   List<Widget> screens = [
     BusinessScreen(),
     SportsScreen(),
@@ -44,11 +49,46 @@ class NewsHandler extends Cubit<NewsState> {
       },
     ).then((value) {
       business = value.data["articles"];
-      print(business[0]["title"]);
       emit(NewsStateGetBusinessSuccess());
     }).catchError((error) {
       print(error.toString());
       emit(NewsStateGetBusinessError(error.toString()));
+    });
+  }
+
+  void getSportsData() {
+    emit(NewsStateGetSportsLoading());
+    DioHelper.getData(
+      path: 'v2/top-headlines',
+      query: {
+        'country': 'eg',
+        'category': 'sports',
+        'apiKey': '65f7f556ec76449fa7dc7c0069f040ca'
+      },
+    ).then((value) {
+      sports = value.data["articles"];
+      emit(NewsStateGetSportsSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(NewsStateGetSportsError(error.toString()));
+    });
+  }
+
+  void getScienceData() {
+    emit(NewsStateGetScienceLoading());
+    DioHelper.getData(
+      path: 'v2/top-headlines',
+      query: {
+        'country': 'eg',
+        'category': 'science',
+        'apiKey': '65f7f556ec76449fa7dc7c0069f040ca'
+      },
+    ).then((value) {
+      science = value.data["articles"];
+      emit(NewsStateGetScienceSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(NewsStateGetScienceError(error.toString()));
     });
   }
 }
