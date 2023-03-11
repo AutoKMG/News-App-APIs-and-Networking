019a6b47f1abf56e79da6bd640fd8049138a7cca
Khaled Saleh <khaled@connou.app>
2023-03-11 17:19:43 +0100

    Building the settings screen and adding switches

--- lib/modules/settings/settings_screen.dart
+++ lib/modules/settings/settings_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/shared/logic/news/handler.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Hello Settings!"));
+    return BlocBuilder<NewsHandler, NewsState>(builder: (context, state) {
+      NewsHandler newsHandler = BlocProvider.of<NewsHandler>(context);
+      return Padding(
+        padding: const EdgeInsets.only(right: 20, left: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Dark Theme',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+                Switch(
+                  value: newsHandler.isDark,
+                  onChanged: (_) {
+                    newsHandler.toggleDarkTheme();
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    'Right-To-Left',
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                ),
+                Switch(
+                  value: newsHandler.isRTL,
+                  onChanged: (_) {
+                    newsHandler.changeDirection();
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
+      );
+    });
   }
 }
