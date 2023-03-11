954ff9d379a466b77de5ff632d070d00545ca3b3
Khaled Saleh <khaled@connou.app>
2023-03-11 17:16:56 +0100

    wrapping material app with bloc consumer and moving app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/news_app/news_layout.dart';
 import 'package:news_app/shared/bloc_observer.dart';
 import 'package:news_app/shared/logic/news/handler.dart';
 import 'package:news_app/shared/network/remote/dio_helper.dart';
+import 'package:news_app/shared/styles/themes.dart';
 
 void main() {
   Bloc.observer = MyBlocObserver();
@@ -26,32 +26,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => NewsHandler(),
-      child: MaterialApp(
-        title: 'News App',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            primarySwatch: Colors.deepOrange,
-            floatingActionButtonTheme: FloatingActionButtonThemeData(
-                backgroundColor: Colors.deepOrange),
-            appBarTheme: AppBarTheme(
-                backgroundColor: Colors.white,
-                systemOverlayStyle: SystemUiOverlayStyle(
-                    statusBarColor: Colors.white,
-                    statusBarIconBrightness: Brightness.dark),
-                elevation: 0.0,
-                iconTheme: IconThemeData(color: Colors.black),
-                titleTextStyle: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black)),
-            scaffoldBackgroundColor: Colors.white,
-            bottomNavigationBarTheme: BottomNavigationBarThemeData(
-              elevation: 20,
-              selectedItemColor: Colors.deepOrange,
-              type: BottomNavigationBarType.fixed,
-            )),
-        home: Directionality(
-            textDirection: TextDirection.rtl, child: NewsLayout()),
+      child: BlocConsumer<NewsHandler, NewsState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          NewsHandler newsHandler = BlocProvider.of<NewsHandler>(context);
+          return MaterialApp(
+            title: 'News App',
+            debugShowCheckedModeBanner: false,
+            themeMode: newsHandler.isDark ? ThemeMode.dark : ThemeMode.light,
+            theme: appLightTheme,
+            darkTheme: appDarkTheme,
+            home: Directionality(
+              textDirection:
+                  newsHandler.isRTL ? TextDirection.rtl : TextDirection.ltr,
+              child: NewsLayout(),
+            ),
+          );
+        },
       ),
     );
   }
