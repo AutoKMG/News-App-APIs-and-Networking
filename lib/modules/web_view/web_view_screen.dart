3371d7fee0a5e1301e8343cac11552b206641cb2
Khaled Saleh <khaled@connou.app>
2023-03-11 22:21:19 +0100

    web view screen to display every article details

---
+++ lib/modules/web_view/web_view_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewScreen extends StatelessWidget {
+  final String url;
+  WebViewScreen(this.url);
+  @override
+  Widget build(BuildContext context) {
+    WebViewController controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {
+            // Update loading bar.
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse(url));
+    return Scaffold(
+      appBar: AppBar(),
+      body: WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
